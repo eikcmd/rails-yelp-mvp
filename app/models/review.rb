933cf3e7d4c9a185c75b6f364d8ab968951c3efb
Eike Feld <eike.feld@gmx.de>
2018-11-14 17:24:29 +0100

    Rails Yelp MWP

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5],
+    message: "%{value} is not a valid rating"}
+end
